fb331b0d9499e0be9f04dec62441d6ff5951a0cc
btaddele <Betelhem.biresaw@gmail.com>
2020-04-13 22:58:41 -0400

    second commit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -9,7 +9,7 @@ public class MyFirstJava {
         public static void main (String[] args)
         {
 
-            System.out.println ("Hello World.");
+            System.out.println ("Hello Betelhem.");
 
         }
 
