8db303ed99f0ea5565363aa6c118269ac091620b
btaddele <Betelhem.biresaw@gmail.com>
2020-04-13 22:47:29 -0400

    First Commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,16 @@
+/*
+this is the  first java program will print out a message Hello world
+
+ */
+
+
+public class MyFirstJava {
+
+        public static void main (String[] args)
+        {
+
+            System.out.println ("Hello World.");
+
+        }
+
+}
